782f509c60f2e4bfcba7c48cfe9cc5003d098717
petarxx99 <104040252+petarxx99@users.noreply.github.com>
2022-05-17 12:59:23 +0200

    Update GlavnaKlasa.java

--- MySQL/assignment1/src/com/glavnipaket/GlavnaKlasa.java
+++ MySQL/assignment1/src/com/glavnipaket/GlavnaKlasa.java
@@ -2,13 +2,11 @@ package com.glavnipaket;
 
 import com.glavnipaket.prozor.GlavniProzor;
 
-import java.math.BigDecimal;
-
 public class GlavnaKlasa {
 
     public static void main(String[] args){
         BazaPodataka baza = new BazaPodataka("assignment_zaposleni", "localhost", 3306,"assignment_user", "password");
-        GlavniProzor glavniProzor = new GlavniProzor(500, 500, baza, "zaposleni");
+        GlavniProzor glavniProzor = new GlavniProzor(500, 500, baza, "zaposleni", 50, 50, new int[]{8, 4});
 
     }
 
