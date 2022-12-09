b21aec9d3820e2dc56cb6d9430a8c655659c3fdf
petarxx99 <104040252+petarxx99@users.noreply.github.com>
2022-12-09 15:24:35 +0100

    Create includeFajl.h

---
+++ polimorfizamC/ZivotinjaPas/includeFajl.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_FAJL
+#define INCLUDE_FAJL
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+#include<stdbool.h>
+
+#define BROJ_BAJTOVA_JEDNOG_KARAKTERA 1
+
+#endif // INCLUDE_FAJL
