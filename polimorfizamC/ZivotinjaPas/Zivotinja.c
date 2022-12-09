a01b6606dcce49e598702d9e229a5c12ed3a64bc
petarxx99 <104040252+petarxx99@users.noreply.github.com>
2022-12-09 15:28:27 +0100

    Create Zivotinja.c

---
+++ polimorfizamC/ZivotinjaPas/Zivotinja.c
@@ -0,0 +1,46 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct ZivotinjaVTableStruct{
+    void (*ispisiPodatkeOZivotinji)(void *zivotinja);
+    int (*dajGodine) (void *zivotinja);
+} ZivotinjaVTable;
+
+
+typedef struct ZivotinjaStruct{
+    ZivotinjaVTable *vTable;
+    int godine;
+} Zivotinja;
+
+
+void ispisiPodatkeOOvojZivotinji(Zivotinja* zivotinja){
+    printf("Ova zivotinja ima %d godina. \n", zivotinja->godine);
+}
+
+int dajGodineOveZivotinje(Zivotinja *z){
+    return z->godine;
+}
+
+void Zivotinja_obrisi(Zivotinja **adresaPointeraKaZivotinji){
+    Zivotinja *zivotinjaZaBrisanje = *adresaPointeraKaZivotinji;
+    free(zivotinjaZaBrisanje);
+    *adresaPointeraKaZivotinji = NULL;
+}
+
+struct ZivotinjaVTableStruct zivotinjaVTableGlobal = {Zivotinja_obrisi, ispisiPodatkeOOvojZivotinji, dajGodineOveZivotinje};
+
+
+Zivotinja* Zivotinja_new(int godine){
+    ZivotinjaVTable *vTable = &zivotinjaVTableGlobal;
+
+    Zivotinja *z = (Zivotinja*) malloc(sizeof(Zivotinja));
+    z->vTable = vTable;
+    z->godine = godine;
+}
+
+
+
+
+
