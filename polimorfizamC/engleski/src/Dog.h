d95dcfa821fa288e7e2621cf9fd1e26a5e9c8e30
petarxx99 <104040252+petarxx99@users.noreply.github.com>
2022-12-14 22:29:08 +0100

    Adding declarations of virtual methods to the Dog.h file
    At first I thought there was no reason for virtual methods to be in the .h file, but I have realized that when child classes don't want to change virtual methods of their parent classes, they should be able to simply add the function from the parent class into their vTableGlobal object instead of copy pasting the body of the function. In order for them to have access to these methods at compile time, virtual methods of the parent class need to be declared in the .h file.

--- polimorfizamC/engleski/src/Dog.h
+++ polimorfizamC/engleski/src/Dog.h
@@ -28,13 +28,17 @@ typedef struct DogStruct{
     CanMakeNoise* (*createCanMakeNoise)(struct DogStruct *_this);
 }Dog;
 
-
+// non virtual methods
 Dog* Dog_new(const char *name, int age, const char *owner, const char *breed);
 
+// methods that are overriden from CanMakeNoise interface
 void Dog_startMakingNoise(Dog *_this, int decibels);
 void Dog_stopMakingNoise(Dog *_this);
 
-
+// virtual methods
+void Dog_delete(Dog **addressToAPointerOfDogToDelete);
+void Dog_doSomething(Dog *_this);
+void Dog_greetOwner(Dog *_this);
 
 #endif // DOG_H
 
